a6906b3c8a196e52c256c857e09b15658ac91809
Hamza <Hamzashafiq259@gmail.com>
2022-08-21 14:37:05 +0500

    integerating firebase

--- src/Screens/orderList/index.js
+++ src/Screens/orderList/index.js
@@ -1,31 +1,52 @@
 import { StyleSheet, Text, View } from "react-native";
 import React, { useState, useEffect } from "react";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
-import { app } from "../../config/firebase";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  query,
+  QuerySnapshot,
+} from "firebase/firestore";
+import { firebase } from "../../config/firebase";
 
 const OrderList = () => {
   const [users, setUsers] = useState();
-  const db = getFirestore();
-  const reef = collection(db, "user");
+  const listRef = firebase.firestore().collection("User");
 
-  useEffect(() => {
-    getDocs(reef)
-      .then((snapshot) => {
-        console.log("-------------", snapshot);
-        let book = [];
-        snapshot.docs.forEach((doc) => {
-          book.push({ ...doc.data() });
-        });
-        console.log(book);
-      })
-      .catch((err) => {
-        console.log(err);
+  useEffect(async () => {
+    listRef.onSnapshot((querySnapshot) => {
+      const list = [];
+      querySnapshot.forEach((doc) => {
+        const { OrderDetails } = doc.data();
+        list.push(OrderDetails);
       });
+      setUsers(list);
+    });
   }, []);
 
+  // const db = getFirestore();
+  // const reef = collection(db, "user");
+
+  // useEffect(() => {()
+  //   getDocs(reef)
+  //     .then((snapshot) => {
+  //       console.log("-------------", snapshot);
+  //       let book = [];
+  //       snapshot.docs.forEach((doc) => {
+  //         book.push({ ...doc.data() });
+  //       });
+  //       console.log(book);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
+
   return (
-    <View>
-      <Text>OrderList</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      {users.map((items) => (
+        <Text>{items}</Text>
+      ))}
     </View>
   );
 };
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,10 +1,28 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBDUpk_pN2TeFHdOWPLWgshX_xqEYTeFlE",
+//   authDomain: "deliveryprime-bdd27.firebaseapp.com",
+//   projectId: "deliveryprime-bdd27",
+//   storageBucket: "deliveryprime-bdd27.appspot.com",
+//   messagingSenderId: "294782217059",
+//   appId: "1:294782217059:web:f32cf7f1417e759f880156",
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+// const authentication = getAuth(app);
+// export default authentication;
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBDUpk_pN2TeFHdOWPLWgshX_xqEYTeFlE",
   authDomain: "deliveryprime-bdd27.firebaseapp.com",
@@ -14,7 +32,8 @@ const firebaseConfig = {
   appId: "1:294782217059:web:f32cf7f1417e759f880156",
 };
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-const authentication = getAuth(app);
-export default authentication;
+if (!firebase.app.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
